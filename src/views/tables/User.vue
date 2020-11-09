9b1113f5fb09085ebb7692ce6387a4f172a027f6
HOLEHUY <you@example.com>
2020-11-09 15:59:20 +0700

    first

--- src/views/tables/User.vue
+++ src/views/tables/User.vue
@@ -5,11 +5,54 @@
       <!-- Full Table -->
       <base-block title="users">
         <template #options>
-          <button type="button" class="btn-block-option">
+          <b-button
+            variant="info"
+            class="d-flex align-items-center"
+            @click="showCreateUser"
+            ><i class="si si-plus pr-2"></i>New user</b-button
+          >
+          <!-- <button type="button" class="btn-block-option">
             <i class="si si-settings"></i>
-          </button>
+          </button> -->
         </template>
-        <b-table-simple responsive bordered striped table-class="table-vcenter">
+        <b-table
+        striped
+        bordered
+          hover
+          :fields="fields"
+          :per-page="perPage"
+          :current-page="currentPage"
+          :items="users"
+        >
+          <template #cell(actions)="user">
+            <b-button
+              size="sm"
+              variant="alt-primary"
+              v-b-modal.modal-edit-user
+              @click="showEditUser(user.item)"
+            >
+              <i class="fa fa-fw fa-pencil-alt"></i>
+            </b-button>
+            <b-button
+              size="sm"
+              variant="alt-danger"
+              v-b-modal.modal-delete-user
+              @click="showDeleteUser(user.item)"
+            >
+              <i class="fa fa-fw fa-times"></i>
+            </b-button>
+          </template>
+        </b-table>
+        <!-- <b-table-simple
+
+          :per-page="perPage"
+          :current-page="currentPage"
+          :items="users"
+          responsive
+          bordered
+          striped
+          table-class="table-vcenter"
+        >
           <b-thead>
             <b-tr>
               <b-th class="text-center" style="width: 100px">
@@ -34,7 +77,7 @@
               </b-td>
               <b-td class="font-w600 font-size-sm">
                 <a :href="`${user.href}`">
-                  {{ user.name }}
+                  {{ user.username }}
                 </a>
               </b-td>
               <b-td class="font-size-sm">
@@ -46,17 +89,180 @@
                 }}</b-badge>
               </b-td>
               <b-td class="text-center">
-                <b-button size="sm" variant="alt-primary">
+                <b-button
+                  size="sm"
+                  variant="alt-primary"
+                  v-b-modal.modal-edit-user
+                  @click="showEditUser(user)"
+                >
                   <i class="fa fa-fw fa-pencil-alt"></i>
                 </b-button>
-                <b-button size="sm" variant="alt-danger">
+                <b-button
+                  size="sm"
+                  variant="alt-danger"
+                  v-b-modal.modal-delete-user
+                  @click="showDeleteUser(user)"
+                >
                   <i class="fa fa-fw fa-times"></i>
                 </b-button>
               </b-td>
             </b-tr>
           </b-tbody>
-        </b-table-simple>
+        </b-table-simple> -->
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+        ></b-pagination>
       </base-block>
+      <b-modal
+        id="modal-delete-user"
+        size="md"
+        body-class="p-0"
+        hide-footer
+        hide-header
+      >
+        <div class="block block-themed block-transparent mb-0">
+          <div class="block-header bg-primary-dark">
+            <h3 class="block-title">Bạn muốn xóa người dùng này không ?</h3>
+          </div>
+          <div class="block-content block-content-full">
+            <div class="row gutters-tiny" style="justify-content: center">
+              <div class="col-auto px-2">
+                <!-- Sales -->
+                <b-button variant="danger" @click="deleteUserConfirm"
+                  >Đồng ý</b-button
+                >
+                <!-- END Sales -->
+              </div>
+              <div class="col-auto px-2">
+                <!-- Payments -->
+                <b-button variant="light" @click="deleteUserConfirm"
+                  >Từ chối</b-button
+                >
+                <!-- END Payments -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </b-modal>
+      <b-modal
+        id="modal-edit-user"
+        size="md"
+        body-class="p-0"
+        hide-footer
+        hide-header
+        @hidden="shouldShowPasswordInput = false"
+      >
+        <div class="block block-themed block-transparent mb-0">
+          <div class="block-header bg-primary-dark">
+            <h3 class="block-title">Chỉnh sửa thông tin người dùng</h3>
+          </div>
+          <div class="block-content block-content-full">
+            <div class="row gutters-tiny" style="justify-content: center">
+              <div class="col-12 px-2">
+                <b-form-group label="Username" label-for="block-form5-username">
+                  <b-form-input
+                    readonly
+                    class="form-control-alt"
+                    :value="currentSelectedUser"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="Password" label-for="block-form5-password">
+                  <b-button
+                    variant="danger"
+                    v-if="!shouldShowPasswordInput"
+                    @click="shouldShowPasswordInput = true"
+                    >Change</b-button
+                  >
+                  <b-form-input
+                    v-else
+                    type="password"
+                    class="form-control-alt"
+                    v-model="form.password"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="Role" label-for="block-form5-username">
+                  <b-form-select
+                    v-model="form.role"
+                    :options="roleOptions"
+                  ></b-form-select>
+                </b-form-group>
+              </div>
+              <div class="col-auto px-2">
+                <!-- Sales -->
+                <b-button @click="editUserConfirm" variant="success"
+                  >Lưu</b-button
+                >
+                <!-- END Sales -->
+              </div>
+              <div class="col-auto px-2">
+                <!-- Payments -->
+                <b-button
+                  variant="light"
+                  @click="$bvModal.hide('modal-edit-user')"
+                  >Hủy</b-button
+                >
+                <!-- END Payments -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </b-modal>
+      <b-modal
+        id="modal-create-user"
+        size="md"
+        body-class="p-0"
+        hide-footer
+        hide-header
+      >
+        <div class="block block-themed block-transparent mb-0">
+          <div class="block-header bg-primary-dark">
+            <h3 class="block-title">Tạo người dùng mới</h3>
+          </div>
+          <div class="block-content block-content-full">
+            <div class="row gutters-tiny" style="justify-content: center">
+              <div class="col-12 px-2">
+                <b-form-group label="Username" label-for="block-form5-username">
+                  <b-form-input
+                    class="form-control-alt"
+                    v-model="form.username"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="Password" label-for="block-form5-password">
+                  <b-form-input
+                    class="form-control-alt"
+                    type="password"
+                    v-model="form.password"
+                  ></b-form-input>
+                </b-form-group>
+                <b-form-group label="Role" label-for="block-form5-username">
+                  <b-form-select
+                    v-model="form.role"
+                    :options="roleOptions"
+                  ></b-form-select>
+                </b-form-group>
+              </div>
+              <div class="col-auto px-2">
+                <!-- Sales -->
+                <b-button @click="createUserConfirm" variant="success"
+                  >Lưu</b-button
+                >
+                <!-- END Sales -->
+              </div>
+              <div class="col-auto px-2">
+                <!-- Payments -->
+                <b-button
+                  variant="light"
+                  @click="$bvModal.hide('modal-create-user')"
+                  >Hủy</b-button
+                >
+                <!-- END Payments -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </b-modal>
       <!-- END Full Table -->
     </div>
   </div>
@@ -66,26 +272,38 @@
 export default {
   data() {
     return {
+      fields: ["username", "role", "created at", "actions"],
+      currentSelectedUser: -1,
+      form: {},
+      currentPage: 1,
+      perPage: 5,
+      shouldShowPasswordInput: false,
+      roleOptions: [
+        { value: "user", text: "User" },
+        { value: "admin", text: "Admin" },
+      ],
       users: [
         {
           id: 1,
-          name: "Adam McCoy",
+          username: "Adam McCoy",
           avatar: "avatar10",
           href: "javascript:void(0)",
           labelVariant: "success",
           labelText: "VIP",
+          actions: null,
         },
         {
           id: 2,
-          name: "Betty Kelley",
+          username: "Betty Kelley",
           avatar: "avatar2",
           href: "javascript:void(0)",
           labelVariant: "info",
           labelText: "Business",
+          actions: null,
         },
         {
           id: 3,
-          name: "Jesse Fisher",
+          username: "Jesse Fisher",
           avatar: "avatar9",
           href: "javascript:void(0)",
           labelVariant: "primary",
@@ -93,7 +311,7 @@ export default {
         },
         {
           id: 4,
-          name: "Ryan Flores",
+          username: "Ryan Flores",
           avatar: "avatar12",
           href: "javascript:void(0)",
           labelVariant: "warning",
@@ -101,7 +319,23 @@ export default {
         },
         {
           id: 5,
-          name: "Alice Moore",
+          username: "Alice Moore",
+          avatar: "avatar4",
+          href: "javascript:void(0)",
+          labelVariant: "danger",
+          labelText: "Disabled",
+        },
+        {
+          id: 6,
+          username: "Alice Moore",
+          avatar: "avatar4",
+          href: "javascript:void(0)",
+          labelVariant: "danger",
+          labelText: "Disabled",
+        },
+        {
+          id: 7,
+          username: "Alice Moore",
           avatar: "avatar4",
           href: "javascript:void(0)",
           labelVariant: "danger",
@@ -109,6 +343,38 @@ export default {
         },
       ],
     };
+  },
+      computed: {
+      rows() {
+        return this.users.length
+      }
+    },
+  methods: {
+    showDeleteUser(user) {
+      this.currentSelectedUser = user.username;
+      console.log(this.currentSelectedUser);
+      // code
+    },
+    deleteUserConfirm() {
+      this.$bvModal.hide("modal-delete-user");
+      this.currentSelectedUser = -1;
+      console.log(this.currentSelectedUser);
+    },
+    showEditUser(user) {
+      console.log(user);
+      this.currentSelectedUser = user.username;
+      //code
+    },
+    editUserConfirm() {
+      this.$bvModal.hide("modal-edit-user");
+    },
+    showCreateUser() {
+      this.$bvModal.show("modal-create-user");
+      this.form = {};
+    },
+    createUserConfirm() {
+      this.$bvModal.hide("modal-create-user");
+    },
   },
 };
 </script>
