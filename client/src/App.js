e0bd5fdee1d890738537b6169b49db170fb23081
NgnaoH <66554255+NgnaoH@users.noreply.github.com>
2020-11-19 21:55:51 +0700

    aloo

--- client/src/App.js
+++ client/src/App.js
@@ -3,36 +3,31 @@ import { Header, Footer } from "./components";
 import Signin from "./page/Sign/Signin";
 import Signup from "./page/Sign/Signup";
 import { Home, Product, About } from "./page";
-import "./App.module.css";
-import { Col, Row } from "antd";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <Row>
-        <Col span={24}>
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/product">
-              <Product />
-            </Route>
-            <Route exact path="/signin">
-              <Signin />
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-            <Route exact path="/About">
-              <About />
-            </Route>
-          </Switch>
-          <Footer />
-        </Col>
-      </Row>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/product">
+          <Product />
+        </Route>
+        <Route exact path="/signin">
+          <Signin />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/About">
+          <About />
+        </Route>
+      </Switch>
+      <Footer />
     </Router>
   );
 };
--- client/src/page/Home/index.js
+++ client/src/page/Home/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "animate.css";
 import { Row, Col } from "antd";
-import { BodyCmt, EndCmt } from "../../components/Comment";
+import { EndCmt } from "../../components/Comment";
 import {
   PromotionsCarousel,
   CommentsCarousel,
