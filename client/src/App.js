216d5443ebd167e1311e305a46b7dad0fc18afc4
NgnaoH <66554255+NgnaoH@users.noreply.github.com>
2020-11-20 12:50:35 +0700

    ád

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,7 @@ import Signup from "./page/Sign/Signup";
 import { Home, Product, About } from "./page";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
 import ShopCart from './page/ShopCart/ShopCart'
-=======
-
->>>>>>> parent of a5fdbcc... push for fun
 const App = () => {
   return (
     <Router>
@@ -26,18 +22,12 @@ const App = () => {
         </Route>
         <Route exact path="/signup">
           <Signup />
-<<<<<<< HEAD
         </Route>
         <Route exact path="/About">
           <About />
         </Route>
         <Route exact path="/shopcart">
           <ShopCart />
-=======
-        </Route>
-        <Route exact path="/About">
-          <About />
->>>>>>> parent of a5fdbcc... push for fun
         </Route>
       </Switch>
       <Footer />
